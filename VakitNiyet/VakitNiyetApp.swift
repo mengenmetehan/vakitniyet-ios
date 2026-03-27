53180298999d19ccdf75dbe43f89c8b87011a3e2
mengen <metehanmengen@gmail.com>
2026-03-27 23:02:51 +0300

    Initial Commit

---
+++ VakitNiyet/VakitNiyetApp.swift
@@ -0,0 +1,17 @@
+//
+//  VakitNiyetApp.swift
+//  VakitNiyet
+//
+//  Created by Metehan Mengen on 27.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct VakitNiyetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
